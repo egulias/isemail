1bacd6dd0d105d5b340392a744fa7bed748e1663
Eduardo Gulias Davis <me@egulias.com>
2012-12-16 18:25:38 +0100

    First version of the lexer
    - New EmailValidator Class
    - New test case

--- src/IsEmail/EmailLexer.php
+++ src/IsEmail/EmailLexer.php
@@ -6,13 +6,77 @@ use JMS\Parser\AbstractLexer;
 
 class EmailLexer extends AbstractLexer
 {
+    const S_AT               = 256;//'@';
+    const S_BACKSLASH        = 257;//'\\';
+    const S_DOT              = 258;//'.';
+    const S_DQUOTE           = 259;//'"';
+    const S_OPENPARENTHESIS  = 260;//'(';
+    const S_CLOSEPARENTHESIS = 261;//')';
+    const S_OPENBRACKET      = 262;//'[';
+    const S_CLOSEBRACKET     = 263;//']';
+    const S_HYPHEN           = 264;//'-';
+    const S_COLON            = 265;//':';
+    const S_DOUBLECOLON      = 266;//'::';
+    const S_SP               = 267;//' ';
+    const S_HTAB             = 268;//"\t";
+    const S_CR               = 269;//"\r";
+    const S_LF               = 270;//"\n";
+    const S_IPV6TAG          = 271;//'IPv6:';
+    const S_LOWERTHAN        = 272;//'<'
+    const S_GREATERTHAN      = 273;//'>'
+    const S_COMMA            = 274;//','
+    const S_SEMICOLON        = 274;//';'
+
+    /**
+     * US-ASCII visible characters not valid for atext (@link http://tools.ietf.org/html/rfc5322#section-3.2.3)
+     *
+     * @var array
+     */
+    protected $nameValue = array(
+        '('    => self::S_OPENPARENTHESIS,
+        ')'    => self::S_CLOSEPARENTHESIS,
+        '<'    => self::S_LOWERTHAN,
+        '>'    => self::S_GREATERTHAN,
+        '['    => self::S_OPENBRACKET,
+        ']'    => self::S_CLOSEBRACKET,
+        ':'    => self::S_COLON,
+        ';'    => self::S_SEMICOLON,
+        '@'    => self::S_AT,
+        '\\'   => self::S_BACKSLASH,
+        ','    => self::S_COMMA,
+        '.'    => self::S_DOT,
+        '"'    => self::S_DQUOTE,
+        '-'    => self::S_HYPHEN,
+        '::'   => self::S_DOUBLECOLON,
+        ' '    => self::S_SP,
+        '\t'   => self::S_HTAB,
+        '\r'   => self::S_CR,
+        '\n'   => self::S_LF,
+        'IPv6' => self::S_IPV6TAG,
+        '<'    => self::S_LOWERTHAN,
+        '>'    => self::S_GREATERTHAN,
+    );
+
+    public function getName($type)
+    {
+        if (isset($this->nameValue[$type])) {
+            $ref = new \ReflectionClass($this);
+            foreach ($ref->getConstants() as $name => $value) {
+                if ($value === $this->nameValue[$type]) {
+                    return $name;
+                }
+            }
+        }
+
+        throw new \InvalidArgumentException(sprintf('There is no token with value %s.', json_encode($value)));
+    }
 
     /**
      * {@inherit}
      */
     protected function getRegex()
     {
-        return '//';
+        return '/\w+/';
     }
 
     /**
@@ -20,11 +84,8 @@ class EmailLexer extends AbstractLexer
      */
     protected function determineTypeAndValue($value)
     {
-        $ref = new \ReflectionClass($this);
-        foreach ($ref->getConstants() as $name => $val) {
-            if ($value === $val) {
-                return array($val, $name);
-            }
+        if (isset($this->nameValue[$value])) {
+            return array($value, $this->nameValue[$value]);
         }
 
         throw new \InvalidArgumentException(sprintf('There is no token with value %s.', json_encode($value)));
---
+++ tests/IsEmail/Tests/EmailValidatorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace IsEmail\Tests;
+
+use IsEmail\EmailValidator;
+
+class EmailValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $validator;
+
+    protected function setUp()
+    {
+        $this->validator = new EmailValidator();
+    }
+
+    protected function tearDown()
+    {
+        $this->validator = null;
+    }
+
+    /**
+     * @dataProvider getValidEmails
+     */
+    public function testValidEmails($email)
+    {
+        $this->assertTrue($this->validator->isValid($email));
+    }
+
+    public function getValidEmails()
+    {
+        return array(
+            array('fabien@symfony.com'),
+//            array('example@example.co.uk'),
+//            array('fabien_potencier@example.fr'),
+//            array('example@localhost'),
+//            array('example((example))@fakedfake.co.uk'),
+        );
+    }
+
+    /**
+     * @dataProvider getInvalidEmails
+     */
+    public function testInvalidEmails($email)
+    {
+        $this->assertFalse($this->validator->isValid($email));
+    }
+
+    public function getInvalidEmails()
+    {
+        return array(
+            array('example.@example.co.uk'),
+//            array('(fabien_potencier@example.fr)'),
+//            array('example(example)example@example.co.uk'),
+//            array('.example@localhost'),
+//            array('ex\ample@localhost'),
+//            array('example@local\host'),
+//            array('example@localhost.'),
+        );
+    }
+}
